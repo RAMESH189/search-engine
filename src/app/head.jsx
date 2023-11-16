5c37ce674097929aada854db6bb66309070393cd
GOD <rameshrajendranb9919@gmail.com>
2023-11-17 01:01:14 +0530

    initial commit - search engine

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>SearchIt</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="search engine" />
+      <link rel="icon" href="/favicon.ico"/>
+    </>
+  )
+}
